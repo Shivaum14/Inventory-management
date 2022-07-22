6e3d753450e689bdf7c034fd48a564615a6217e7
Shiv <Shivaumm@gmail.com>
2022-07-22 19:58:54 +0100

    7th commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
 import "./App.css";
-import Info from "./Info.js";
+import { PropTypes } from "prop-types";
+import { useState } from "react";
+import Info, { Info1 } from "./Info.js"; //imports default and non default
+import SearchBar from "./SearchBar.js";
 
 function App() {
+	const [data, setData] = useState({});
+
+	const updateData = (searchParams) => {
+		setData(searchParams);
+	};
 	return (
 		<div className="App">
-			<Info />
-			<Additem />
-			<Additem />
-			<Additem />
+			<Info1 />
+			<br />
+			<SearchBar callback={updateData} />
+			<p>Name: {"name" in data ? data["name"] : "No Data to display"}</p>
+			<p>Max Price: {"price" in data ? data["price"] : "No Data to display"}</p>
+			<p>Type: {"type" in data ? data["type"] : "No Data to display"}</p>
+			<p>Brand: {"brand" in data ? data["brand"] : "No Data to display"}</p>
+			<br />
+			<br />
+			<Info title="Inventory Management" />
+			<br />
+			<br />
+			<ButtonState />
+			<br />
+			<br />
+			<AddItem text="Shiv" number={14} />
+			<br />
+			<AddItem />
+			<br />
+			<br />
+		</div>
+	);
+}
+function ButtonState() {
+	const [title, setTitle] = useState("base title");
+	const [count, setCount] = useState(0);
+
+	const updateTitleClicked = () => {
+		setTitle(title + "!");
+	};
+	const updateCountClicked = () => {
+		setCount(count + 1);
+	};
+
+	return (
+		<div>
+			<Data title={title} count={count} />
+			<button onClick={updateTitleClicked}>Update Title</button>
+			<button onClick={updateCountClicked}>Update Counter</button>
 		</div>
 	);
 }
 
-function Additem() {
-	const value = " DEFAULT ";
+function Data(props) {
+	return (
+		<div>
+			<p>Title: {props.title}</p>
+			<p>Count: {props.count}</p>
+		</div>
+	);
+}
+
+function AddItem(props) {
+	const value = props.text;
 	return (
 		<form>
-			<label for="text-form">Type something: </label>
-			<input type="text" value={value} id="text-form" />
+			<label htmlFor="text-form">Type something: </label>
+			<input type="text" defaultValue={value} id="text-form" />
+			<p>{props.number}</p>
 		</form>
 	);
 }
+
+//defines default props
+AddItem.defaultProps = {
+	number: 2,
+	text: "<-------->",
+};
+
+//defines default datatypes for props
+AddItem.propTypes = {
+	number: PropTypes.number,
+};
 
 export default App;
--- src/Info.js
+++ src/Info.js
@@ -1,50 +1,51 @@
 import React from "react";
 
-// class based component
+// class based component with stste
 class Info extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			count: 0,
+			title: "Hello",
+		};
+	}
+
+	buttonPressed() {
+		this.setState({
+			count: this.state.count + 1,
+		});
+	}
+
 	render() {
-		const title = "Inventory Management";
-		const showTitle = true;
-		if (showTitle) {
-			return (
-				<div>
-					<h1>{title}</h1>
-					<p>Manage your stuff.</p>
-				</div>
-			);
-		} else {
-			return (
-				<div>
-					<p>showTitle = false</p>
-				</div>
-			);
-		}
+		return (
+			<div>
+				<p>Count: {this.state.count}</p>
+				<button onClick={() => this.buttonPressed()}>Click Me!</button>
+			</div>
+		);
 	}
 }
 
-/*
 //functional component
-function Info() {
+function Info1() {
 	const title = "Inventory Management";
 	const showTitle = true;
 
-	if(showTitle){
-		return(
+	if (showTitle) {
+		return (
 			<div>
 				<h1>{title}</h1>
 				<p>Manage your stuff.</p>
 			</div>
 		);
-
-	} else{
-		return(
+	} else {
+		return (
 			<div>
 				<p>showTitle = false</p>
 			</div>
-		)
+		);
 	}
-
 }
-*/
 
 export default Info;
+export { Info1 };
